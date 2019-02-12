9dc749d768697b7d729cf6a475e63ff7d1950626
Marcel Stör <marcelstoer@users.noreply.github.com>
2019-02-12 22:18:06 +0100

    Bump versions

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 #rm -fr build dist
+VERSION=4.0
+NAME=NodeMCU-PyFlasher
+
 pyinstaller --log-level=DEBUG \
             --noconfirm \
             build-on-mac.spec
 
 #https://github.com/sindresorhus/create-dmg
-#
-#create-dmg NodeMCU-PyFlasher-3.0.app
+create-dmg dist/$NAME-$VERSION.app
+mv "$NAME-$VERSION 0.0.0.dmg" dist/$NAME-$VERSION.dmg
